0cb3bb05e905a440fe57a2964d940f69454e3048
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-02-23 19:02:51 -0300

    Add files via upload

---
+++ Soma/Soma.java
@@ -0,0 +1,37 @@
+package Soma;
+
+import javax.swing.JOptionPane;
+
+/*
+*
+* @ Ailton Lima de Andrade
+*/
+public class Soma{
+    /*
+    * Método: main()
+    * @param: String [] args: vetor de argumentos fornecidos na linha de comando durante a chamada do sistema.
+    * @return: nenhum valor de retorno.
+    * Funcionalidades:
+    *       Realizar a leitura de um nome e a soma de dois números e
+    *       apresentar o nome digitado pelo usuário e o resultado da 
+    *       soma dos dois números.
+    */ 
+    public static void main(String[] args) {
+        //Declaração das váriaveis
+        String nome;
+        double primeiroValor, segundoValor, soma; 
+        
+        //Faz a leitura do nome
+        nome = JOptionPane.showInputDialog(null, "Digite o seu nome:", "nome",JOptionPane.INFORMATION_MESSAGE);
+        
+        //Faz a leitura dos números
+        primeiroValor = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o primeiro número: ", "Primeiro Número",JOptionPane.INFORMATION_MESSAGE));
+        segundoValor = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o segundo número: ", "Segundo Número",JOptionPane.INFORMATION_MESSAGE));
+        
+        //Efetua a soma dos números digitados
+        soma = primeiroValor + segundoValor;
+        
+        //Acresenta o nome e a soma dos números digitados
+        System.out.println("O meu nome é: " + nome + " o valor da soma é: " + soma);
+    } 
+}
